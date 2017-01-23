330edc4959919545e0c6f69eabd37ef6deb4ed1e
Andrew Thompson <andrew@tobarja.com>
2017-01-22 20:36:55 -0500

    Project Setup

--- ForgetTheMilk/App_Start/RouteConfig.cs
+++ ForgetTheMilk/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ForgetTheMilk
@@ -16,7 +12,7 @@ namespace ForgetTheMilk
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Task", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class TaskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
