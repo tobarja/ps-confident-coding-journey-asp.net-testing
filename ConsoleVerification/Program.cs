8939db12e525bf68978cd1694aed814660494148
Andrew Thompson <andrew@tobarja.com>
2017-01-22 21:31:13 -0500

    What if Console Verifies, Too?

--- ConsoleVerification/Program.cs
+++ ConsoleVerification/Program.cs
@@ -9,16 +9,34 @@ namespace ConsoleVerification
         {
             var input = "Pickup the groceries";
             var task = new Task(input);
-            Console.WriteLine("Description: " + task.Description);
-            Console.WriteLine("Due Date: " + task.DueDate);
-            Console.WriteLine();
+            var descriptionShouldBe = input;
+            DateTime? dueDateShouldBe = null;
+            Console.WriteLine("Scenario: " + input);
+            if (descriptionShouldBe == task.Description && dueDateShouldBe == task.DueDate)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("Description: " + task.Description + " should be " + descriptionShouldBe);
+                Console.WriteLine("Due Date: " + task.DueDate + " should be " + dueDateShouldBe);
+                Console.WriteLine("ERROR");
+            }
 
             input = "Pickup the groceries may 5";
             task = new Task(input);
-            Console.WriteLine("Description: " + task.Description);
-            Console.WriteLine("Due Date: " + task.DueDate);
-            Console.WriteLine();
-
+            dueDateShouldBe = new DateTime(DateTime.Today.Year, 5, 5);
+            Console.WriteLine("Scenario: " + input);
+            if (dueDateShouldBe == task.DueDate)
+            {
+                Console.WriteLine("SUCCESS");
+            }
+            else
+            {
+                Console.WriteLine("Description: " + task.Description + " should be " + descriptionShouldBe);
+                Console.WriteLine("Due Date: " + task.DueDate + " should be " + dueDateShouldBe);
+                Console.WriteLine("ERROR");
+            }
             Console.ReadLine();
         }
     }
