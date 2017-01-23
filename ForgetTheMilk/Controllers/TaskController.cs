ed82813788ac1494ad5a48f9aff9215eab87c3ad
Andrew Thompson <andrew@tobarja.com>
2017-01-22 21:04:51 -0500

    Parsing out a Due Date

--- ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using ForgetTheMilk.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace ForgetTheMilk.Controllers
 {
     public class TaskController : Controller
     {
-        public static readonly List<string> Tasks = new List<string>();
+        public static readonly List<Task> Tasks = new List<Task>();
 
         public ActionResult Index()
         {
@@ -15,7 +18,17 @@ namespace ForgetTheMilk.Controllers
         [HttpPost]
         public ActionResult Add(string task)
         {
-            Tasks.Add(task);
+            var newTask = new Task { Description = task };
+            var dueDatePattern = new Regex(@"may\s(\d)");
+            var hasDueDate = dueDatePattern.IsMatch(task);
+            if (hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+                newTask.DueDate = new DateTime(DateTime.Today.Year, 5, day);
+            }
+
+            Tasks.Add(newTask);
             return RedirectToAction("Index");
         }
     }
---
+++ ForgetTheMilk/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ForgetTheMilk.Models
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
