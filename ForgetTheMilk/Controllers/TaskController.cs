360b447daa3ad461446a0406b68ffc3f6fb49e51
Andrew Thompson <andrew@tobarja.com>
2017-01-22 20:45:52 -0500

    Add and Show Tasks

--- ForgetTheMilk/Controllers/TaskController.cs
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -1,12 +1,22 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ForgetTheMilk.Controllers
 {
     public class TaskController : Controller
     {
+        public static readonly List<string> Tasks = new List<string>();
+
         public ActionResult Index()
         {
-            return View();
+            return View(Tasks);
+        }
+
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            Tasks.Add(task);
+            return RedirectToAction("Index");
         }
     }
 }
