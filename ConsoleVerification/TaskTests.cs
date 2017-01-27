2a5a28c60d88d6428b27df1c3a9af52d90497d7c
Andrew Thompson <andrew@tobarja.com>
2017-01-26 22:08:20 -0500

    Module: Using a Testing Framework

---
+++ ConsoleVerification/TaskTests.cs
@@ -0,0 +1,44 @@
+﻿using ForgetTheMilk.Models;
+using NUnit.Framework;
+using System;
+
+namespace ConsoleVerification
+{
+    class TaskTests : AssertionHelper
+    {
+        [Test]
+        public void DescriptionAndNoDueDate()
+        {
+            var input = "Pickup the groceries";
+            Console.WriteLine("Scenario: " + input);
+
+            var task = new Task(input, default(DateTime));
+
+            Expect(task.Description, Is.EqualTo(input));
+            Expect(task.DueDate, Is.Null);
+        }
+
+        [Test]
+        public void TestMayDueDateDoesWrapYear()
+        {
+            var input = "Pickup the groceries may 5 - as of 2015-05-31";
+            var today = new DateTime(2015, 5, 31);
+
+            var task = new Task(input, today);
+
+            Expect(task.DueDate, Is.EqualTo(new DateTime(2016, 5, 5)));
+        }
+
+        [Test]
+        public void TestMayDueDateDoesNotWrapYear()
+        {
+            var input = "Pickup the groceries may 5 - as of 2015-05-04";
+            Console.WriteLine("Scenario: " + input);
+            var today = new DateTime(2015, 5, 4);
+
+            var task = new Task(input, today);
+
+            Expect(task.DueDate, Is.EqualTo(new DateTime(2015, 5, 5)));
+        }
+    }
+}
